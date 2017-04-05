318fb29914ca008629276ac3772f3469ff561469
Daniel Tull <dt@danieltull.co.uk>
2017-04-05 11:35:16 +0100

    Add UI test

--- UI Test/UITest.swift
+++ UI Test/UITest.swift
@@ -6,6 +6,31 @@ class UITest: XCTestCase {
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
-        XCUIApplication().launch()
+
+		let app = XCUIApplication()
+		app.launch()
+
+		addUIInterruptionMonitor(withDescription: "Location Dialog") { alert -> Bool in
+			alert.buttons["Allow"].tap()
+			return true
+		}
+
+		// Causes the interuption above to fire.
+		app.tap()
     }
+
+	func testApp() {
+
+		let app = XCUIApplication()
+		let latitude = app.staticTexts.matching(identifier: "latitude").element
+		let longitude = app.staticTexts.matching(identifier: "longitude").element
+
+		let exists = NSPredicate(format: "exists == 1")
+		expectation(for: exists, evaluatedWith: latitude, handler: nil)
+		expectation(for: exists, evaluatedWith: longitude, handler: nil)
+		waitForExpectations(timeout: 10, handler: nil)
+
+		XCTAssertEqual(latitude.label, "51.50998")
+		XCTAssertEqual(longitude.label, "-0.1337")
+	}
 }

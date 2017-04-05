380fd14fa9f13740b6d1f602f57f2eb0860d6485
Daniel Tull <dt@danieltull.co.uk>
2017-04-05 11:12:08 +0100

    Initial Commit

---
+++ Location Command Line Build Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Location Command Line Build Test
+//
+//  Created by Daniel Tull on 05.04.2017.
+//  Copyright © 2017 Daniel Tull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+

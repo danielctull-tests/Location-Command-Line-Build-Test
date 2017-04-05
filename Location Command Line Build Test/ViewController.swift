10c185f04aead948dc1e8700e35bd2f450aa1339
Daniel Tull <dt@danieltull.co.uk>
2017-04-05 11:26:29 +0100

    Add code for amazing app

--- Location Command Line Build Test/ViewController.swift
+++ Location Command Line Build Test/ViewController.swift
@@ -1,5 +1,44 @@
 
+import CoreLocation
 import UIKit
 
 class ViewController: UIViewController {
+
+	private let locationManager = CLLocationManager()
+
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		locationManager.delegate = self
+		locationManager.startUpdatingLocation()
+	}
+
+	@IBOutlet private var latitudeLabel: UILabel? { didSet { updateLabels() }}
+	@IBOutlet private var longitudeLabel: UILabel? { didSet { updateLabels() }}
+	fileprivate var location: CLLocation? { didSet { updateLabels() } }
+
+	private func updateLabels() {
+
+		guard
+			let latitudeLabel = latitudeLabel,
+			let longitudeLabel = longitudeLabel,
+			let location = location
+		else {
+			self.latitudeLabel?.text = ""
+			self.longitudeLabel?.text = ""
+			return
+		}
+
+		latitudeLabel.text = String(location.coordinate.latitude)
+		longitudeLabel.text = String(location.coordinate.longitude)
+	}
+}
+
+
+
+extension ViewController: CLLocationManagerDelegate {
+
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+		location = locations.last
+	}
 }
